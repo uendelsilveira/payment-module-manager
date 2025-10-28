12baa1e67ffe229fe9a2d651d87ebc35d315a880
uendelsilveira <uendel.silveira@gmail.com>
2025-10-28 19:13:20 -0300

    Add Payment facade alias in composer.json and update file header in Payment.php

--- src/Facades/Payment.php
+++ src/Facades/Payment.php
@@ -4,7 +4,7 @@
  By Uendel Silveira
  Developer Web
  IDE: PhpStorm
- Created: 28/10/2025 20:43:22
+ Created: 28/10/2025 20:43:21
 */
 
 namespace UendelSilveira\PaymentModuleManager\Facades;
@@ -13,13 +13,14 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \UendelSilveira\PaymentModuleManager\Models\Transaction processPayment(array $data)
- *
  * @see \UendelSilveira\PaymentModuleManager\Services\PaymentService
  */
 class Payment extends Facade
 {
     /**
      * Get the registered name of the component.
+     *
+     * @return string
      */
     protected static function getFacadeAccessor(): string
     {
